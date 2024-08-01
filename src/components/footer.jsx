59364339190f6cd7bc8e6e43c7fa0d9089b94663
maameyaa <evelynyaabaidoo@gmail.com>
2024-08-01 14:38:05 +0000

    task

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+  <div>
+    <h3>TaskerHub</h3>
+    <h4>Discover</h4>
+    <a href="Explore Services">All Services</a> <br />
+    <a href="Tasker">Become a Tasker</a>
+
+  </div>
+
+    </div>
   )
 }
 
